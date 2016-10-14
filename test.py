5e4a267c087018e1aacbd0e01a6d42c0298147db
volitilov <volitilov@gmail.com>
2016-10-14 21:28:10 +0300

    lesson-3

--- test.py
+++ test.py
@@ -52,14 +52,58 @@
 # print(test.capitalize())	# переводит первый символ строки в верхний регистр, а все остальные в нижний
 
 # ////////////////////////////////////////////
-
 # форматирование строк
-import random
 
-test0 = random.randrange(1, 3)
-test1 = 'jhdjf'
-test2 = 2
-test3 = len(test1)
+# import random
 
-print("test: {test[0]}".format(test = [test0, test1, test2, test3]))
-print('{2},{0},{1}'.format('a', 'b', 'c'))
+# test0 = random.randrange(1, 3)
+# test1 = 'jhdjf'
+# test2 = 2
+# test3 = len(test1)
+
+# print("test: {test[0]}".format(test = [test0, test1, test2, test3]))
+# print('{2},{0},{1}'.format('a', 'b', 'c'))
+
+# print('{:>30}'.format('right align'))
+# print('{:^30}'.format('center'))
+
+# points = 19.5
+# total = 22
+
+# print('test: {:.2%}'.format(points/total))
+
+# //////////////////////////////////////////////
+# списки
+
+# методы списков
+# list.append(x)		Добавляет элемент в конец списка
+# list.extend(L)		Расширяет список list, добавляя в конец все элементы списка L
+# list.insert(i, x)		Вставляет на i-ый элемент значение x
+# list.remove(x)		Удаляет первый элемент в списке, имеющий значение x. ValueError, если такого элемента не существует
+# list.pop([i])			Удаляет i-ый элемент и возвращает его. Если индекс не указан, удаляется последний элемент
+# list.count(x)			Возвращает количество элементов со значением x
+# list.reverse()		Разворачивает список
+# list.copy()			Поверхностная копия списка
+# list.clear()			Очищает список
+
+# list.index(x, [start, [end]])		Возвращает положение первого элемента со значением x 
+# 									(при этом поиск ведется от start до end)
+# list.sort([key = function])		Сортирует список на основе функции
+
+# print(list("test")) 	# ['t', 'e', 's', 't']
+
+# s = [] 	# пустой список 
+
+# test = ['e', 'f', ['var'], 3]
+
+# # генератор списков
+# test2 = [c * 3 for c in 'test']
+# test3 = [c * 3 for c in 'test3' if c != '3']
+
+# s1 = ['ewu', 'wety', test, 987]
+# s.extend(s1)
+# print(s)
+
+# /////////////////////////////////////////////////////
+# индексы и срезы
+
