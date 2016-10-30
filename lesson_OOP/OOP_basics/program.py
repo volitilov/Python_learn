d7233c75219092499777267d21502a68fcc63b63
volitilov <volitilov@gmail.com>
2016-10-30 20:52:07 +0300

    add program lesson_OOP/OOP_basics

---
+++ lesson_OOP/OOP_basics/program.py
@@ -0,0 +1,12 @@
+#	/////////////////////////////////////////////////////////
+#	Here, the practice on the topic "OOP_basics" ............
+
+#	Task:
+
+#	Написать простой клас с методом и атрибутом
+
+class FirstClass:
+	test = 'python'
+
+	def g(self):
+		return self.test
---
+++ lesson_OOP/OOP_basics/test.py
@@ -0,0 +1,6 @@
+from program import FirstClass as Fc
+
+a = Fc()
+a.test = 'lalka'
+
+print(a.g())
