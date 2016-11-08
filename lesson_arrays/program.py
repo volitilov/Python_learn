fbda954810c06b0d6a8eb31cf5ca59530dd4090e
volitilov <volitilov@gmail.com>
2016-11-08 21:19:19 +0300

    learn_arrays

---
+++ lesson_arrays/program.py
@@ -0,0 +1,32 @@
+#   //////////////////////////////////////////////////////////
+#   program test learn .......................................
+
+#	
+a = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+
+#	example 1
+# for i in range(len(a)):
+# 	for j in range(len(a[i])):
+# 		print(a[i][j], end=' ')
+# 	print()
+
+#	example 2
+# s = 0
+# for x in a:
+# 	for t in x:
+# 		s += t 
+# 	print(s)
+
+#	example 3
+# n = 5
+# m = 5
+# a = [0] * n
+# for i in range(n):
+#     a[i] = [0] * m
+# print(a)
+
+#	example 4
+n = 3
+m = 3
+a = [[' '] * m for i in range(n)]
+print(a)
