06efc21e8b07b64034546e2be36199c4c05a8ac8
volitilov <volitilov@gmail.com>
2018-01-21 12:18:59 +0300

    добавил нифу: Beautiful_Soup

--- library/third-party/parsing_and_scraping/Beautiful_Soup/ex.py
+++ library/third-party/parsing_and_scraping/Beautiful_Soup/ex.py
@@ -1,7 +1,25 @@
-import requests
+# import requests
 from bs4 import BeautifulSoup
 
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+BeautifulSoup("<a></p>", "html.parser")
+# <a></a>
+
+BeautifulSoup("<a></p>", "html5lib")
+# <html><head></head><body><a><p></p></a></body></html>
+
+BeautifulSoup("<a></p>", "lxml")
+# <html><body><a></a></body></html>
+
+BeautifulSoup("<a><b /></a>", "xml")
+# <?xml version="1.0" encoding="utf-8"?>
+# <a><b/></a>
+
+BeautifulSoup("<a><b /></a>")
+# <html><head></head><body><a><b></b></a></body></html>
+
+
 
 # res = requests.get('https://vk.com/volitilov')
 
@@ -14,46 +32,40 @@ from bs4 import BeautifulSoup
 # 	print('Ошибка: {}'.format(exc))
 
 
-with open('ex.html') as file:
+with open('example.html') as file:
 	soup = BeautifulSoup(file.read(), 'html.parser')
 	# print(soup.prettify())
-	
 	# print(soup.title)
-	# <title>Вячеслав Волитилов</title>
-
 	# print(soup.title.string)
-	# Вячеслав Волитилов
-
 	# print(soup.title.parent.name)
-	# head
-
 	# print(soup.a)
-	# <a accesskey="*" class="header__left hb_wrap mhb_home mhb_vkhome" href="/">
-	# 	<div class="hb_btn mhi_home mhi_vkhome"> </div>
-	# </a>
-
 	# print(soup.a['class'])
-	# ['header__left', 'hb_wrap', 'mhb_home', 'mhb_vkhome']
-
 	# print(soup.find_all('a'))
-	# [<a class="post__anchor anchor" name="post115067466_719"></a>, 
-	#	<a href="/volitilov">
-	#		<img class="wi_img" src="https://pp.userapi.com/c840227/v840227446/49fb3/QjMvyfzJU8Q.jpg"/>
-	# 	</a>
-	# ]
-
 	# print(soup.find(id='img_ex'))
-	# <img class="wi_img" id="img_ex" src="ex.jpg"/>
-
 	# for link in soup.find_all('a'):
 	# 	print(link.get('href'))
-		# None
-		# /volitilov
 
 	# print(soup.get_text())
-	# Вячеслав Волитилов
 
-	tag = soup.img
-	print(tag.name) # img
-	print(tag['class']) # ['wi_img']
-	print(tag.attrs) # {'id': 'img_ex', 'src': 'ex.jpg', 'class': ['wi_img']}
+	# tag = soup.img
+	# tag.name = 'a'
+	# tag['id'] = 'brand'
+	# tag['class'] = ['brand_image', 'image']
+	# print(tag)
+	# tag.name = 'img'
+	# print(tag)
+	# del tag['id']
+	# print(tag)
+	# print(tag.name)
+	# print(tag['class'])
+	# print(tag.attrs)
+
+	# print(soup.img['class'])
+	# print(soup.img.attrs)
+
+	# print(soup.span.string)
+	# soup.span.string.replace_with('experement_text')
+	# print(soup.span.string)
+
+	btn = soup.find_all(attrs={"type": "button"})
+	print(btn)
